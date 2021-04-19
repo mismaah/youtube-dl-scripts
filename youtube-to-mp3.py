9813b786c4cf00ae5e5cec6ff671dd56f3a837bd
Mismaah Abdulla <mismaahabdulla@gmail.com>
2021-04-19 13:46:18 +0500

    only download audio

--- youtube-to-mp3.py
+++ youtube-to-mp3.py
@@ -7,5 +7,5 @@ import subprocess
 while True:
     url = input("Enter youtube url: ")
     subprocess.run(
-        f'youtube-dl {url} -x --audio-format mp3 --audio-quality 0 --embed-thumbnail --postprocessor-args "-id3v2_version 3"'
+        f'youtube-dl -f bestaudio {url} -x --audio-format mp3 --audio-quality 0 --embed-thumbnail --postprocessor-args "-id3v2_version 3"'
     )
